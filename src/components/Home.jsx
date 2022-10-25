c96defad7e333c71f596948d972331c7a748ebd2
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-10-25 12:20:20 +0200

    Added Redux toolkit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { useGetAllProductsQuery } from '../slices/productsApi';
+
 
 const Home = () => {
+  const { data, error, isLoading } = useGetAllProductsQuery();
   return (
-      <div>
-          <h2>Home</h2>
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An Error occurred</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product">
+                <h3>{ product.name }</h3>
+                <img src={ product.image } alt={ product.name } />
+                <div className="details">
+                  <span>{ product.desc }</span>
+                  <span className='price'>${ product.price }</span>
+                  <button>Add To Cart</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default Home
--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import productsReducer, { productsFetch } from './slices/productsSlice';
+import { productsApi } from './slices/productsApi';
+// import cartReducer, { getTotals } from './slices/cartSlice';
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    // cart: cartReducer,
+    [ productsApi.reducerPath ]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+store.dispatch(productsFetch());
+// store.dispatch(getTotals());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store } >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
